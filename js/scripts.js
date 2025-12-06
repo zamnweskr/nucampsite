10272a1872def29a44debe324b34b98ba967d194
Ryan Girard <rmg9923@gmail.com>
2025-12-06 01:54:23 -0500

    Changed the button on carousel to be play/pause

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
 console.log('JavaScript Connected!');
-//console.log('Bootstrap Available?', typeof bootstrap);
-
-//const carouselElement = document.querySelector('#homeCarousel');
-//console.log('Carousel Element:', carouselElement);
 
 const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
+    interval: 5000,
     pause: false
 })
 
-//console.log('Carousel Instance:', carousel);
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
 
-//when pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
-})
-
-//when play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
-    carousel.cycle();
+carouselButton.addEventListener('click', function() {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
 })
